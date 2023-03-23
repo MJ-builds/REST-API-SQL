152f8a36b5e7e374c51bfe138edafbc8d6b0e776
MJ <112978328+MJ-builds@users.noreply.github.com>
2023-03-23 23:07:49 +0200

    init routes.js

---
+++ routes.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+
+const { User, Course } = require('./models');
+
+
+// Construct a router instance.
+const router = express.Router();
+
+module.exports = router;
