d24b727008c69b15312d7bce0c0319c1a8dba11e
MJ <112978328+MJ-builds@users.noreply.github.com>
2023-03-28 21:48:21 +0200

    update

--- routes.js
+++ routes.js
@@ -22,7 +22,10 @@ function asyncHandler(cb) {
 }
 
 //GET - return the currently authenticated user
-router.get("/users", authenticateUser, asyncHandler(async (req, res) => {
+router.get(
+  "/users",
+  authenticateUser,
+  asyncHandler(async (req, res) => {
     const user = req.currentUser;
     res.status(200).json({
       id: user.id,
@@ -34,7 +37,9 @@ router.get("/users", authenticateUser, asyncHandler(async (req, res) => {
 );
 
 //POST - create users
-router.post("/users",asyncHandler(async (req, res) => {
+router.post(
+  "/users",
+  asyncHandler(async (req, res) => {
     try {
       // Create a new user using the extracted data
       await User.create(req.body);
@@ -42,7 +47,10 @@ router.post("/users",asyncHandler(async (req, res) => {
       // Return 201 status/no content
       res.status(201).end();
     } catch (error) {
-      if (error.name === "SequelizeValidationError" || error.name === "SequelizeUniqueConstraintError") {
+      if (
+        error.name === "SequelizeValidationError" ||
+        error.name === "SequelizeUniqueConstraintError"
+      ) {
         const errors = error.errors.map((err) => err.message);
         res.status(400).json({ errors });
       } else {
@@ -53,32 +61,63 @@ router.post("/users",asyncHandler(async (req, res) => {
 );
 
 //GET all courses - no authentication required here
-router.get("/courses", asyncHandler(async (req, res) => {
+router.get(
+  "/courses",
+  asyncHandler(async (req, res) => {
     const courses = await Course.findAll({
       attributes: {
         exclude: ["createdAt", "updatedAt"],
       },
+      include: [{ model: User, as: "user" }],
     });
     res.status(200).json(courses);
-    }));
+  })
+);
+
+//GET course by id - no authentication required here
+router.get(
+  "/courses/:id",
+  asyncHandler(async (req, res) => {
+    //TODO: Amend to try and catch if needed
+    const courseId = req.params.id;
+    const course = await Course.findByPk(courseId, {
+      attributes: {
+        exclude: ["createdAt", "updatedAt"],
+      },
+      include: [{ model: User, as: "user" }],
+    });
+    if (course) {
+      res.status(200).json(course);
+    } else {
+      res.status(404).json({ message: "Course not found" });
+    }
+  })
+);
 
 //POST - create courses - authentication required
-router.post("/courses", authenticateUser, asyncHandler(async (req, res) => {
+router.post(
+  "/courses",
+  authenticateUser,
+  asyncHandler(async (req, res) => {
     try {
-        // Create a new course using the extracted data
-        await Course.create(req.body);
-        //TODO: To be amended to the /courses/id route (for the new course created)
-        res.location("/");
-        // Return 201 status/no content
-        res.status(201).end();
-      } catch (error) {
-        if (error.name === "SequelizeValidationError" || error.name === "SequelizeUniqueConstraintError") {
-          const errors = error.errors.map((err) => err.message);
-          res.status(400).json({ errors });
-        } else {
-          throw error;
-        }
+      // Create a new course using the extracted data
+      const course = await Course.create(req.body);
+      //TODO: confirm /api included. to test once router.get for the course is created
+      res.location(`/courses/${course.id}`);
+      // Return 201 status/no content
+      res.status(201).end();
+    } catch (error) {
+      if (
+        error.name === "SequelizeValidationError" ||
+        error.name === "SequelizeUniqueConstraintError"
+      ) {
+        const errors = error.errors.map((err) => err.message);
+        res.status(400).json({ errors });
+      } else {
+        throw error;
       }
-}));
+    }
+  })
+);
 
 module.exports = router;
